61f58c8d30c35b46b2239ebac3368790a043f281
Adam Wagner <awagner83@gmail.com>
2014-03-29 23:14:59 -0400

    Added amalgamate-version of dust's shootout benchmark.

---
+++ suites/amalgamate_suite.js
@@ -0,0 +1,81 @@
+(function(exports){
+
+var benches = {
+
+  string: {
+    source:  "Hello World!",
+    context: {}
+  },
+
+  replace: {
+    source:  "Hello {{name}}! You have {{count}} new messages.",
+    context: { name: "Mick", count: 30 }
+  },
+
+  array: {
+    source:  "{{#names}}{{name}}{{/names}}",
+    context: { names: [
+                 { name: "Moe" },
+                 { name: "Larry" },
+                 { name: "Curly" },
+                 { name: "Shemp" }
+               ]
+             }
+  },
+
+  object: {
+    source:  "{{#person}}{{name}}{{age}}{{/person}}",
+    context: { person: { name: "Larry", age: 45 } }
+  },
+
+  filter: {
+      source: "FOO {{bar|filter}}",
+      context: {
+          filter: function(text) { return text.toUpperCase(); },
+          bar: 'bar'
+      }
+  },
+
+  complex: {
+    source:  "<h1>{{null|header}}</h1>\n"                          +
+             "{{?items}}\n"                                        +
+             "  <ul>\n"                                            +
+             "    {{#items}}\n"                                    +
+             "      {{?current}}\n"                                +
+             "        <li><strong>{{name}}</strong></li>\n"        +
+             "      {{/current}}{{^current}}\n"                    +
+             "        <li><a href=\"{{url}}\">{{name}}</a></li>\n" +
+             "      {{/current}}\n"                                +
+             "    {{/items}}\n"                                    +
+             "  </ul>\n"                                           +
+             "{{/items}}{{^items}}\n"                              +
+             "  <p>The list is empty.</p>\n"                       +
+             "{{/items}}",
+    context: {
+               header: function() {
+                 return "Colors";
+               },
+               items: [
+                 {name: "red", current: true, url: "#Red"},
+                 {name: "green", current: false, url: "#Green"},
+                 {name: "blue", current: false, url: "#Blue"}
+               ]
+             }
+  }
+
+};
+
+exports.amalgamateBench = function(suite, name, id) {
+    var bench = benches[name],
+        ctx = bench.context;
+
+    tpl = Amalgamate.load(Amalgamate.compile(bench.source));
+    suite.bench(id || name, function(next) {
+        Amalgamate.render(tpl, ctx);
+        next();
+    });
+};
+
+exports.amalgamateBench.benches = benches
+
+})(typeof exports !== 'undefined' ? exports: window);
